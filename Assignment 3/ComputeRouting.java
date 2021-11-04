d10a060c86bf94500e9ecd04a3a6b84619aa08f4
Wenxi Chen <johnchen77w@gmail.com>
2021-11-04 19:48:04 -0400

    Final commit, everything is working
    Yay but I am still single

--- Assignment 3/ComputeRouting.java
+++ Assignment 3/ComputeRouting.java
@@ -1,61 +1,58 @@
 public class ComputeRouting extends Algorithm {
 
-    public Object run() {
-        // Our sumTree algorithm returns the total of distances from the descendants to
-        // this node
-        String result = computeRoutingTable(getID());
-        return result;
-    }
+	public Object run() {
+		// Our sumTree algorithm returns the total of distances from the descendants to
+		// this node
+		String result = computeRoutingTable(getID());
+		return result;
+	}
 
-    public String computeRoutingTable(String id) {
-        try {
-            RoutingTable table = new RoutingTable(id);
+	public String computeRoutingTable(String id) {
+		try {
+			RoutingTable table = new RoutingTable(id);
+			Message mssg = null;
+			Message message = null;
+			int count = 0;
+			// Start at leaf
+			if (numChildren() == 0 && !isRoot())
+				mssg = makeMessage(getParent(), table.stringRepresentation(id, table));
+			else
+				mssg = null;
 
-            /* Your initialization code goes here */
-            Message mssg, message;
-            String[] data;
-            int rounds_left = -1;
-            int count = 0;
-
-            if (numChildren() == 0) {
-                mssg = makeMessage(getParent(), table.stringRepresentation(id, table));
-            } else {
-                mssg = null;
-            }
-
-            while (waitForNextRound()) { /* synchronous loop */
-                /* Your algorithm goes here */
-                if (mssg != null) {
-                    send(mssg);
-                    rounds_left = 0;
-                }
-                // Receive message
-                message = receive();
-                while (message != null) {
-                    data = message.data().split(" ");
-                    System.out.println(message.data());
-                    for (int i = 2; i < data.length; i += 2) {
-                        table.addEntry(data[0], data[i]);
-                    }
-                    table.addEntry(data[0], data[0]);
-                    count++;
-                    message = receive();
-                }
-                if (numChildren() == count) { // All children at this rank have been added
-                    if (isRoot()) { // We arrived at root
-                        table.printTables();
-                        return "";
-                    } else { // We are not at root yet
-                        mssg = makeMessage(getParent(), table.stringRepresentation(id, table)); // Send mssg up
-                    }
-                }
-                if (rounds_left == 0) {
-                    return "";
-                }
-            }
-        } catch (SimulatorException e) {
-            System.out.println("ERROR: " + e.toString());
-        }
-        return "";
-    }
+			while (waitForNextRound()) {
+				if (mssg != null)
+					send(mssg);
+				mssg = null;
+				// All children of this level has been added
+				if (count == numChildren()) {
+					if (isRoot()) // If at root, return table and terminate
+						table.printTables();
+					else // If not at root, send message up to parent
+						mssg = makeMessage(getParent(), table.stringRepresentation(id, table));
+					return ""; // For simulator
+				}
+				// Receive message
+				message = receive();
+				while (message != null) {
+					count++;
+					// Sender info added, hop and destination are the same
+					table.addEntry(message.source(), message.source());
+					if (table.emptyRoutingTable(message.data()) && !isRoot()) // Received empty table from leaf
+						mssg = makeMessage(getParent(), table.stringRepresentation(id, table));
+					else { // Received non-empty table somewhere above the leaf
+							// Iterate the message and populate our own table
+						for (int i = 0; i < table.numEntries(message.data()); i++)
+							// Keeping the source as destination but next hop set to our source
+							table.addEntry(message.source(), table.getDestination(message.data(), i));
+						if (!isRoot()) // If not at root, send message up to parent
+							mssg = makeMessage(getParent(), table.stringRepresentation(id, table));
+					}
+					message = receive(); // Receive message (again)
+				}
+			}
+		} catch (SimulatorException e) {
+			System.out.println("ERROR: " + e.toString());
+		}
+		return "";
+	}
 }
